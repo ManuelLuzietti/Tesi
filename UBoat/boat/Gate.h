97595a1978635e36da48083b5d354bf384161573
ManuelLuzietti <manuel.luzietti@studio.unibo.it>
2023-04-13 11:12:51 +0200

    add UBoat bot used for testing, modified

---
+++ UBoat/boat/Gate.h
@@ -0,0 +1,13 @@
+#ifndef GATE_H
+#define GATE_H
+//Gate 1
+static const char* GATE_HOST = "100.100.100.151";
+//static const char* GATE_PATH = "/css_r/gate.php";
+static const char* GATE_PATH = "/gate.php";
+//Gate 2
+static const char* FALLBACK_GATE_HOST = "100.100.100.151"; // here is your game 
+static const char* FALLBACK_GATE_PATH = "/UBoat/gate.php";
+
+static const int POLL_RATE = (1000 * 63 * 1);
+
+#endif
